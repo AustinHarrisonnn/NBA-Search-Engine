33b87608d3db6aeab0ace72c526b72f8b77b0f2a
Austin Harrison <austin.harrisonn@yahoo.com>
2024-11-13 17:22:47 -0500

    added basketball image assets modified gui.py to better format application

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
 from nba_scraper import get_player_stats
 
 def search_player_stats():
@@ -14,18 +16,30 @@ def search_player_stats():
 
 
 root = tk.Tk()
+root.geometry('600x400')
 root.title("NBA Player Stats Search")
 
+image = Image.open(r'C:\Users\ausgood\Desktop\NBATrack\src\assets\basketballIMG.png')
+image = image.resize((130, 100), Image.ANTIALIAS)
+image = ImageTk.PhotoImage(image)
+
+image_label = tk.Label(root, image=image)
+image_label.image = image  # Prevent garbage collection
+image_label.grid(row=0, column=0, padx=10, pady=10, sticky='nw')
+
 player_name_label = tk.Label(root, text="Enter Player's Name:")
-player_name_label.pack()
+player_name_label.grid(row=0, column=1, sticky='w')
 
 player_name_entry = tk.Entry(root)
-player_name_entry.pack()
+player_name_entry.grid(row=0, column=2, padx=5, pady=5, sticky='ew')
 
 search_button = tk.Button(root, text="Search", command=search_player_stats)
-search_button.pack()
+search_button.grid(row=0, column=3, padx=10, pady=5, sticky='e')
+
+root.grid_columnconfigure(2, weight=1)
+root.grid_columnconfigure(3, weight=1)
 
 result_label = tk.Label(root, text="Player stats will be displayed here.")
-result_label.pack()
+result_label.grid(row=1, column=0, columnspan=4, pady=30)
 
 root.mainloop()
